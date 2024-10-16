cc98b58e97e3000ee7ad81c7a5bab634f4351343
JuliannaMali <mali.julianna6@gmail.com>
2024-10-16 17:16:37 +0200

    funkcje-lab2

--- PP-Types/Program.cs
+++ PP-Types/Program.cs
@@ -1,7 +1,4 @@
-﻿// Write required code.
-
-// Data - do not change it in code!
-string[] names = {
+﻿string[] names = {
     "Mickey Mouse", "Minnie Mouse", "Donald Duck", "Goofy", "Pluto", "Daisy Duck", "Simba", "Nala", 
     "Timon", "Pumbaa", "Mufasa", "Ariel", "Flounder", "Sebastian", "Ursula", "Belle", "Beast", "Gaston", 
     "Cinderella", "Prince Charming", "Aurora", "Maleficent", "Rapunzel", "Flynn Rider", "Elsa", "Anna", 
@@ -9,108 +6,96 @@ string[] names = {
 };
 
 
-// Print all array elements, *perLine* elements per one line
-// After all elements except last one should be ", " - also on the end of lines.
-// After last element should be ".".
 void PrintGroups(string[] t, int perLine)
 {
+    int count = 0;
+    foreach (var name in t)
+    {
+        if (name != t[^1])
+        {
+            if (count < perLine)
+            {
+                Console.Write(name + ", ");
+                count++;
 
-    // Write required code.
+            }
+            else if (count == perLine)
+            {
+                Console.Write($"\n{name}, ");
+                count = 1;
+            }
+        }
+        else
+        {
+            Console.Write(name + ". ");
+        }
+
+    }
 
 }
 
-
-// Print all array elements in *perLine* columns.
-// Each column must have given *width* (number of chars).
-// Columns should be separated by "| ".
-// If element is too long it should be trimmed.
 
 void PrintColumns(string[] t, int perLine, int width)
 {
 
-    // Write required code.
+    int count = 0;
+
+    foreach(var name in t)
+    {
+        if (count < perLine)
+        {
+            count++;
+            if (name.Length <= width)
+            {
+                Console.Write(name + new string(' ', (width - name.Length)) + "|");
+            }
+            else
+            {
+                Console.Write(name[..width] + "|");
+            }
+        }
+
+        else if (count == perLine)
+        {
+            count = 1;
+            if (name.Length <= width)
+            {
+                Console.Write($"\n{name}" + new string(' ', (width - name.Length)) +"|");
+            }
+            else
+            {
+                Console.Write($"\n{name[..width]}" + "|");
+            }
+
+        }
+    }
 
 }
 
 
-// Test how your functions work. 
-// You can temprary comment some lines not needed for current testing.
-
-
 Console.WriteLine("\nPrintGroups(names, 3):\n");
 PrintGroups(names, 3);
-/*
-Mickey Mouse, Minnie Mouse, Donald Duck,
-Goofy, Pluto, Daisy Duck,
-Simba, Nala, Timon,
-Pumbaa, Mufasa, Ariel,
-Flounder, Sebastian, Ursula,
-Belle, Beast, Gaston,
-Cinderella, Prince Charming, Aurora,
-Maleficent, Rapunzel, Flynn Rider,
-Elsa, Anna, Olaf,
-Moana, Maui, Hercules.
-*/
+
 
 Console.WriteLine("\nPrintGroups(names, 5):\n");
 PrintGroups(names, 5);
-/*
-Mickey Mouse, Minnie Mouse, Donald Duck, Goofy, Pluto,
-Daisy Duck, Simba, Nala, Timon, Pumbaa,
-Mufasa, Ariel, Flounder, Sebastian, Ursula,
-Belle, Beast, Gaston, Cinderella, Prince Charming,
-Aurora, Maleficent, Rapunzel, Flynn Rider, Elsa,
-Anna, Olaf, Moana, Maui, Hercules. 
-*/
+
 
 Console.WriteLine("\nPrintGroups(names, 7):\n");
 PrintGroups(names, 7);
-/*
-Mickey Mouse, Minnie Mouse, Donald Duck, Goofy, Pluto, Daisy Duck, Simba,
-Nala, Timon, Pumbaa, Mufasa, Ariel, Flounder, Sebastian,
-Ursula, Belle, Beast, Gaston, Cinderella, Prince Charming, Aurora,
-Maleficent, Rapunzel, Flynn Rider, Elsa, Anna, Olaf, Moana,
-Maui, Hercules.
-*/
 
-// For width = 40 don't worry if result will be wrapped due to screen width.
+
 Console.WriteLine("\nPrintGroups(names, 40):\n");
 PrintGroups(names, 40);
-/*
-Mickey Mouse, Minnie Mouse, Donald Duck, Goofy, Pluto, Daisy Duck, Simba, Nala, Timon, Pumbaa, Mufasa, Ariel, Flounder,
-Sebastian, Ursula, Belle, Beast, Gaston, Cinderella, Prince Charming, Aurora, Maleficent, Rapunzel, Flynn Rider, Elsa, A
-nna, Olaf, Moana, Maui, Hercules.
-*/
+
 
 Console.WriteLine("\n\nPrintColumns(names, 4, 17):\n");
 PrintColumns(names, 4, 17);
-/*
-Mickey Mouse     | Minnie Mouse     | Donald Duck      | Goofy
-Pluto            | Daisy Duck       | Simba            | Nala
-Timon            | Pumbaa           | Mufasa           | Ariel
-Flounder         | Sebastian        | Ursula           | Belle
-Beast            | Gaston           | Cinderella       | Prince Charming
-Aurora           | Maleficent       | Rapunzel         | Flynn Rider
-Elsa             | Anna             | Olaf             | Moana
-Maui             | Hercules         |
-*/
+
 
 Console.WriteLine("\n\nPrintColumns(names, 5, 15):\n");
 PrintColumns(names, 5, 15);
-/*
-Mickey Mouse   | Minnie Mouse   | Donald Duck    | Goofy          | Pluto
-Daisy Duck     | Simba          | Nala           | Timon          | Pumbaa
-Mufasa         | Ariel          | Flounder       | Sebastian      | Ursula
-Belle          | Beast          | Gaston         | Cinderella     | Prince Charming
-Aurora         | Maleficent     | Rapunzel       | Flynn Rider    | Elsa
-Anna           | Olaf           | Moana          | Maui           | Hercules
-*/
+
 
 Console.WriteLine("\n\nPrintColumns(names, 7, 10):\n");
-PrintColumns(names, 8, 10);
-/*
-Mickey Mou| Minnie Mou| Donald Duc| Goofy     | Pluto     | Daisy Duck| Simba     | Nala
-Timon     | Pumbaa    | Mufasa    | Ariel     | Flounder  | Sebastian | Ursula    | Belle
-Beast     | Gaston    | Cinderella| Prince Cha| Aurora    | Maleficent| Rapunzel  | Flynn Ride
-Elsa      | Anna      | Olaf      | Moana     | Maui      | Hercules  |
-*/
+PrintColumns(names, 7, 10);
